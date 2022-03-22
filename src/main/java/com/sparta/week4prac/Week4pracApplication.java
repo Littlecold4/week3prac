f00a871b8d1b85539047deb9b861723110caf410
LIttlecold4 <minu09252@gmail.com>
2022-03-22 12:20:23 +0900

    firstcommit

---
+++ src/main/java/com/sparta/week4prac/Week4pracApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week4prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4pracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4pracApplication.class, args);
+    }
+
+}
